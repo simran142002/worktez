d175f7a72ff13d470c57d6a5549611603c4cf349
Simran Nigam <nigamsimran14@gmail.com>
2022-08-20 23:28:18 +0530

    made changes

--- functions/model/users/tark/checkIfUserAlreadyAMember.js
+++ functions/model/users/tark/checkIfUserAlreadyAMember.js
@@ -1,9 +1,31 @@
+/* eslint-disable linebreak-style */
+/** *********************************************************
+ * Copyright (C) 2022
+ * Worktez
+ * Author: Simran Nigam <nigamsimran14@gmail.com>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the MIT License
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the MIT License for more details.
+ ***********************************************************/
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-undef */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable max-len */
+// eslint-disable-next-line no-dupe-else-if
+
 const {getTeam} = require("../../teams/lib");
 
 exports.checkIfUserAlreadyAMember = function(request, response) {
   const organizationDomain = request.body.data.OrganizationDomain;
   const teamName = request.body.data.TeamName;
-  //   const teamId =request.body.data.TeamId;
   const userEmail = request.body.data.UserEmail;
   let status = 200;
   let resultData = "";
